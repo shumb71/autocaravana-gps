a9294fecbc86d8e1af1fbdb87d163daf6c404b05
shumb71 <shumb71@gmail.com>
2026-05-22 03:43:32 +0200

    Update fetch_gps.py

--- fetch_gps.py
+++ fetch_gps.py
@@ -1,1 +1,27 @@
+import json, os, sys
+from datetime import datetime, timezone
 
+body = os.environ.get('RMS_BODY', '')
+if not body:
+    sys.exit(1)
+
+d = json.loads(body)['data']
+out = {
+    "lat": d['latitude'],
+    "lon": d['longitude'],
+    "satellites": d['satellites'],
+    "accuracy": d['accuracy'],
+    "altitude": d['altitude'],
+    "speed": d['speed'],
+    "heading": d['course'],
+    "temperature": d['temperature'],
+    "signal": d['signal'],
+    "operator": d['operator'],
+    "timestamp": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
+    "source": "rms"
+}
+
+with open('gps.json', 'w') as f:
+    json.dump(out, f)
+
+print(f"GPS: {d['latitude']}, {d['longitude']}")
